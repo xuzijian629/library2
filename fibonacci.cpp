666110872e7b2b30db7666149bcda8fda2cb2119
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2018-10-31 20:35:19 +0900

    add fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -12,58 +12,70 @@ struct mat {
     mat() {
         d = vvi(n, vi(n));
     }
-
+    
     mat(initializer_list<initializer_list<i64>> m) {
-        for (auto a: m) {
+        for (auto& a: m) {
             vi row(a.begin(), a.end());
             d.emplace_back(row);
         }
         assert(n == d.size());
         assert(n == d.front().size());
     };
-
-    mat operator+(const mat rhs) {
+    
+    mat operator+(const mat& rhs) {
         mat ret;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 ret.d[i][j] = d[i][j] + rhs.d[i][j];
+                ret.d[i][j] %= MOD;
+                ret.d[i][j] += MOD;
+                ret.d[i][j] %= MOD;
             }
         }
         return ret;
     }
-
-    mat operator-(const mat rhs) {
+    
+    mat operator-(const mat& rhs) {
         mat ret;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 ret.d[i][j] = d[i][j] - rhs.d[i][j];
+                ret.d[i][j] %= MOD;
+                ret.d[i][j] += MOD;
+                ret.d[i][j] %= MOD;
             }
         }
         return ret;
     }
-
-    mat operator*(const mat rhs) {
+    
+    mat operator*(const mat& rhs) {
         mat ret;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     ret.d[i][j] += d[i][k] * rhs.d[k][j];
+                    ret.d[i][j] %= MOD;
+                    ret.d[i][j] += MOD;
+                    ret.d[i][j] %= MOD;
                 }
             }
         }
         return ret;
     }
-
+    
     mat operator*(const i64 k) {
         mat ret;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 ret.d[i][j] = d[i][j] * k;
+                ret.d[i][j] %= MOD;
+                ret.d[i][j] += MOD;
+                ret.d[i][j] %= MOD;
             }
         }
         return ret;
     }
-
+    
     static mat eye() {
         mat ret;
         for (int i = 0; i < n; i++) {
@@ -74,7 +86,7 @@ struct mat {
 };
 
 template<int k>
-mat<k> pow(mat<k> a, i64 n) {
+mat<k> pow(mat<k>& a, i64 n) {
     if (n == 0) {
         return mat<k>::eye();
     }
@@ -89,7 +101,7 @@ i64 fib(i64 n) {
     if (n <= 1) return n;
     mat<2> f{{1, 1}, {1, 0}};
     mat<2> res = pow(f, n - 2);
-    return res.d[0][0] + res.d[0][1];
+    return (res.d[0][0] + res.d[0][1]) % MOD;
 }
 
 int main() {
