2c957688077352ec6f6db86520ae2ad46e864a26
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2018-11-14 17:46:49 +0900

    add matrix

--- matrix.cpp
+++ matrix.cpp
@@ -3,97 +3,303 @@ using namespace std;
 using i64 = int64_t;
 using vi = vector<i64>;
 using vvi = vector<vi>;
+constexpr long double EPS = 1e-12;
 constexpr i64 MOD = 1e9 + 7;
 
-template<int n>
-struct mat {
-    vvi d;
-
-    mat() {
-        d = vvi(n, vi(n));
+template<typename T>
+class mat : public vector<vector<T>> {
+private:
+    int r, c;
+public:
+    int row() const {
+        return r;
     }
-
-    mat(initializer_list<initializer_list<i64>> m) {
-        for (auto a: m) {
-            vi row(a.begin(), a.end());
-            d.emplace_back(row);
-        }
-        assert(n == d.size());
-        assert(n == d.front().size());
-    };
-
-    mat operator+(const mat rhs) {
-        mat ret;
+    int column() const {
+        return c;
+    }
+    mat(int n, int m, T val = 0) {
+        r = n;
+        c = m;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ret.d[i][j] = d[i][j] + rhs.d[i][j];
+            this->push_back(vector<T>(m, val));
+        }
+    }
+    mat operator+(const mat& rhs) {
+        assert(r == rhs.r && c == rhs.c);
+        mat<T> ret(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ret[i][j] = (*this)[i][j] + rhs[i][j];
             }
         }
         return ret;
     }
-
-    mat operator-(const mat rhs) {
-        mat ret;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ret.d[i][j] = d[i][j] - rhs.d[i][j];
+    mat operator+(const T val) {
+        mat<T> ret(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ret[i][j] = (*this)[i][j] + val;
             }
         }
         return ret;
     }
-
-    mat operator*(const mat rhs) {
-        mat ret;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    ret.d[i][j] += d[i][k] * rhs.d[k][j];
+    mat operator-(const mat& rhs) {
+        assert(r == rhs.r && c == rhs.c);
+        mat<T> ret(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ret[i][j] = (*this)[i][j] - rhs[i][j];
+            }
+        }
+        return ret;
+    }
+    mat operator-(const T val) {
+        mat<T> ret(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ret[i][j] = (*this)[i][j] - val;
+            }
+        }
+        return ret;
+    }
+    vector<T> operator*(const vector<T>& rhs) {
+        assert(c == rhs.size());
+        vector<T> vec(r, 0);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                vec[i] += (*this)[i][j] * rhs[j];
+            }
+        }
+        return vec;
+    }
+    mat operator*(const mat& rhs) {
+        assert(c == rhs.r);
+        mat<T> ret(r, rhs.c);
+        for (int i = 0; i < r; i++) {
+            for (int k = 0; k < c; k++) {
+                for (int j = 0; j < rhs.c; j++) {
+                    ret[i][j] += (*this)[i][k] * rhs[k][j];
                 }
             }
         }
         return ret;
     }
-
-    mat operator*(const i64 k) {
-        mat ret;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ret.d[i][j] = d[i][j] * k;
+    mat operator-() {
+        mat<T> ret(r, c);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ret[i][j] = -(*this)[i][j];
             }
         }
         return ret;
     }
-
-    static mat eye() {
-        mat ret;
-        for (int i = 0; i < n; i++) {
-            ret.d[i][i] = 1;
+	int rank() {
+	    int res = 0;
+	    mat<double> B(r, c);
+	    for (int i = 0; i < r; i++) {
+	        for (int j = 0; j < c; j++) {
+				B[i][j] = (*this)[i][j];
+			}
+	    }
+	    for (int i = 0; i < c; i++) {
+	        if (res == r) return res;
+	        int pivot = res;
+	        for (int j = res + 1; j < r; j++) {
+	            if (abs(B[j][i]) > abs(B[pivot][i])) {
+	                pivot = j;
+	            }
+	        }
+	        if (abs(B[pivot][i]) < EPS) continue;
+	        swap(B[pivot], B[res]);
+	        for (int j = i + 1; j < c; j++) {
+	            B[res][j] /= B[res][i];
+	        }
+	        for (int j = res + 1; j < r; j++) {
+	            for (int k = i + 1; k < c; k++) {
+	                B[j][k] -= B[res][k]*B[j][i];
+	            }
+	        }
+	        res++;
+	    }
+	    return res;
+	}
+    T det() {
+        assert(r == c);
+        T ans = 1;
+        mat B(r, r);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < r; j++) {
+                B[i][j] = (*this)[i][j];
+            }
         }
-        return ret;
+        for (int i = 0; i < r; i++) {
+            for (int j = i + 1; j < r; j++) {
+                for (; B[j][i] != 0; ans = -ans) {
+                    T tm = B[i][i] / B[j][i];
+                    for (int k = i; k < r; k++) {
+                        T t = B[i][k] - tm * B[j][k];
+                        B[i][k] = B[j][k];
+                        B[j][k] = t;
+                    }
+                }
+           }
+           ans *= B[i][i];
+       }
+       return ans;
+    }
+    mat inverse() {
+        assert(r == c);
+        mat B(r, 2 * r);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < r; j++) {
+                B[i][j] = (*this)[i][j];
+            }
+        }
+        for (int i = 0; i < r; i++) {
+            B[i][r + i] = 1;
+        }
+        for (int i = 0; i < r; i++) {
+            int pivot = i;
+            for (int j = i; j < r; j++) {
+                if (abs(B[j][i]) > abs(B[pivot][i])) {
+                    pivot = j;
+                }
+            }
+
+            // assert regular
+            assert(abs(B[pivot][i]) > EPS);
+            swap(B[i], B[pivot]);
+            for (int j = i + 1; j <= 2 * r; j++) {
+                B[i][j] /= B[i][i];
+            }
+            for (int j = 0; j < r; j++) {
+                if (i != j) {
+                    for (int k = i + 1; k <= 2*r; k++) {
+                        B[j][k] -= B[j][i] * B[i][k];
+                    }
+                }
+            }
+        }
+        mat res(r, r);
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < r; j++) {
+                res[i][j] = B[i][r + j];
+            }
+        }
+        return res;
+    }
+    void print() {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c - 1; j++) {
+                cout << (*this)[i][j] << '\t';
+            }
+            cout << (*this)[i][c - 1] << endl;
+        }
     }
 };
 
-template<int k>
-mat<k> pow(mat<k> a, i64 n) {
-    if (n == 0) {
-        return mat<k>::eye();
+template<typename T>
+vector<T> eq_solve(const mat<T>& A,const vector<T>& b) {
+    assert(A.row() == A.column());
+    int n = A.row();
+    mat<T> B(n, n + 1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            B[i][j] = A[i][j];
+        }
     }
-    if (n % 2 == 0) {
-        mat<k> t = pow(a, n / 2);
-        return t * t;
+    for (int i = 0; i < n; i++) {
+        B[i][n] = b[i];
     }
-    return a * pow(a, n - 1);
+    for (int i = 0; i < n; i++) {
+        int pivot = i;
+        for (int j = i; j < n; j++) {
+            if (abs(B[j][i]) > abs(B[pivot][i])) {
+                pivot = j;
+            }
+        }
+        // assert having a unique root
+        assert(abs(B[pivot][i]) > EPS);
+        swap(B[i], B[pivot]);
+        for (int j = i + 1; j <= n; j++) {
+            B[i][j] /= B[i][i];
+        }
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                for (int k = i + 1; k <= n; k++) {
+                    B[j][k] -= B[j][i] * B[i][k];
+                }
+            }
+        }
+    }
+    vector<T> ret(n);
+    for (int i = 0; i < n; i++) {
+        ret[i] = B[i][n];
+    }
+    return ret;
 }
 
-i64 fib(i64 n) {
-    if (n <= 1) return n;
-    mat<2> f{{1, 1}, {1, 0}};
-    mat<2> res = pow(f, n - 2);
-    return res.d[0][0] + res.d[0][1];
+template<typename T>
+mat<T> pow(mat<T> A, i64 k) {
+    assert(A.row() == A.column());
+    int n = A.row();
+	mat<T> B(n, n);
+	for (int i = 0; i < n; i++) {
+		B[i][i] = 1;
+	}
+	while (k > 0) {
+		if (k & 1) {
+			B = B * A;
+		}
+		A = A * A;
+		k >>= 1;
+	}
+	return B;
+}
+
+template<typename T>
+mat<T> mod_mul(mat<T>& A, mat<T>& B) {
+    assert(A.column() == B.row());
+    mat<T> ret(A.row(), B.column());
+    for (int i = 0; i < A.row(); i++) {
+        for (int k = 0; k < A.column(); k++) {
+            for (int j = 0; j < B.column(); j++) {
+                ret[i][j] = (ret[i][j] + A[i][k]*B[k][j]) % MOD;
+            }
+        }
+    }
+    return ret;
+}
+
+template<typename T>
+mat<T> mod_pow(mat<T> A, i64 k) {
+    assert(A.row() == A.column());
+    int n = A.row();
+	mat<T> B(n, n);
+	for (int i = 0; i < n; i++) {
+		B[i][i] = 1;
+	}
+	while (k > 0) {
+		if (k & 1) {
+			B = mod_mul(B, A);
+		}
+		A = mod_mul(A, A);
+		k >>= 1;
+	}
+	return B;
 }
 
 int main() {
     int n;
     cin >> n;
-    cout << fib(n + 1) << endl;
+    mat<i64> m(n, n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            char c;
+            cin >> c;
+            m[i][j] = c - '0';
+        }
+    }
+
+    cout << (m.det() % 2 ? "Odd" : "Even") << endl;
 }
