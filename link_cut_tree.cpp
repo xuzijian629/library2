46e19d498eaf9551131e58561e9885107186cb3d
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2018-11-15 11:44:34 +0900

    add link cut tree

--- link_cut_tree.cpp
+++ link_cut_tree.cpp
@@ -4,242 +4,209 @@ using i64 = int64_t;
 using vi = vector<i64>;
 using vvi = vector<vi>;
 
-// 頂点クエリに答える用
+template <class T, class T2 = T>
 struct LinkCutTree {
-    static const int L = 0, R = 1;
-    struct node {
-        node *pp, *cp[2]; // *lp, *rp
+    using F = function<T(T, T)>;
+    using G = function<T(T, T2, int)>;
+    using H = function<T2(T2, T2)>;
+
+    struct Node {
+        Node *l, *r, *p;
+        int idx;
+        T key, sum;
+        T2 lazy;
+
         bool rev;
-        int dcost, dmin;
-        int id;
-        node() {
-            id = -1;
-            pp = cp[L] = cp[R] = nullptr;
-            rev = false;
-            dcost = dmin = 0;
-        }
-        
+        int sz;
+
         bool is_root() {
-            return !pp || pp->cp[L] != this && pp->cp[R] != this;
+            return !p || (p->l != this && p->r != this);
         }
 
-        void propagate() {
-            if (rev) {
-                swap(cp[L], cp[R]);
-                if (cp[L]) {
-                    cp[L]->rev ^= true;
-                }
-                if (cp[R]) {
-                    cp[R]->rev ^= true;
-                }
-                rev = false;
-            }
-        }
-
-        // rot_right if d == 1, rot_left if d == 0
-        void rot(int d) {
-            node *q = pp, *r = q->pp;
-            int e = 1 - d;
-            if (q->cp[e] = cp[d]) {
-                cp[d]->pp = q;
-            }
-            cp[d] = q; q->pp = this;
-
-            int qd = q->dmin;
-            q->dmin = q->dcost;
-            if (q->cp[e]) {
-                q->dmin = min(q->dmin, dmin + q->cp[e]->dmin);
-            }
-            if (q->cp[d]) {
-                q->dmin = min(q->dmin, q->cp[d]->dmin);
-            }
-            q->dcost -= q->dmin;
-            if (q->cp[e]) {
-                q->cp[e]->dmin += dmin - q->dmin;
-            }
-            if (q->cp[d]) {
-                q->cp[d]->dmin -= q->dmin;
-            }
-            if (cp[e]) {
-                cp[e]->dmin += dmin;
-            }
-            dcost += dmin;
-            dmin = qd;
-            
-            if (pp=r) {
-                if (r->cp[L] == q) {
-                    r->cp[L] = this;
-                }
-                if (r->cp[R] == q) {
-                    r->cp[R] = this;
-                }
-            }
-        }
-        
-        void splay() {
-            propagate();
-            while (!is_root()) {
-                node *q = pp;
-                if (q->is_root()) {
-                    q->propagate();
-                    propagate();
-                    if (q->cp[L] == this) {
-                        rot(R);
-                    } else {
-                        rot(L);
-                    }
-                } else{
-                    node *r = q->pp;
-                    r->propagate();
-                    q->propagate();
-                    propagate();
-                    for (int i = 0; i < 2; i++) { 
-                        if (r->cp[i] == q) {
-                            if (q->cp[i] == this) {
-                                q->rot(1-i);
-                                rot(1-i);
-                            } else {
-                                rot(i);
-                                rot(1-i);
-                            }
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+        Node(int idx, const T &key, const T2 &v) : idx(idx), key(key), sum(key), lazy(v), sz(1), l(nullptr), r(nullptr), p(nullptr), rev(false) {}
     };
 
-    vector<node> nodes;
-    LinkCutTree(int n) : nodes(n) {
-        for (int i = 0; i < n; i++) {
-            nodes[i].id = i;
-        }
-    }
-    node *expose(node *x) {
-        node *rp = NULL;
-        for (node *p = x; p != NULL; p = p->pp) {
-            int ndmin;
-            p->splay();
+    const T e;
+    const T2 e2;
+    const F f;
+    const G g;
+    const H h;
 
-            if (p->cp[R]) {
-                p->cp[R]->dmin += p->dmin;
-            }
-            ndmin = p->dmin + p->dcost;
-            if (rp) {
-                ndmin = min(ndmin, rp->dmin);
-            }
-            if (p->cp[L]) {
-                ndmin = min(ndmin, p->dmin + p->cp[L]->dmin);
-            }
-            if (rp) {
-                rp->dmin = rp->dmin - ndmin;
-            }
-            if (p->cp[L]) {
-                p->cp[L]->dmin += p->dmin - ndmin;
-            }
-            p->dcost += p->dmin - ndmin;
-            p->dmin = ndmin;
+    LinkCutTree(const F &f, const T &e) : LinkCutTree(f, G(), H(), e, T2()) {}
+    LinkCutTree(const F &f, const G &g, const H &h, const T &e, const T2 &e2) : f(f), g(g), h(h), e(e), e2(e2) {}
 
-            p->cp[R] = rp;
-            rp = p;
-        }
-        x->splay();
-        return x;
+    Node *make_node(int idx, const T &v = T()) {
+        return new Node(idx, v, e2);
     }
-    int findroot(int n) {
-        node *v = expose(&nodes[n]);
-        while (v->cp[L]) {
-            v->propagate();
-            v = v->cp[L];
-        }
-        v->splay();
-        return v->id;
+
+    void propagate(Node *t, const T2 &x) {
+        t->lazy = h(t->lazy, x);
+        t->key = g(t->key, x, 1);
+        t->sum = g(t->sum, x, t->sz);
     }
-    // vから根に向かうパスの中で最小の重みとその重みをもつ最も根に近い頂点の組を返す
-    pair<int, int> findcost(int n) {
-        node *v = expose(&nodes[n]);
-        while (1) {
-            v->propagate();
-            if (v->cp[L] && v->cp[L]->dmin == 0) {
-                v = v->cp[L];
-            } else if ((!v->cp[L] || v->cp[L]->dmin > 0) && v->dcost > 0) {
-                v = v->cp[R];
+
+    void toggle(Node *t) {
+        swap(t->l, t->r);    
+        t->rev ^= true;
+    }
+
+    void push(Node *t) {
+        if (t->lazy != e2) {
+            if (t->l) propagate(t->l, t->lazy);
+            if (t->r) propagate(t->r, t->lazy);
+            t->lazy = e2;
+        }
+        if (t->rev) {
+            if (t->l) toggle(t->l);
+            if (t->r) toggle(t->r);
+            t->rev = false;
+        }
+    }
+
+    void update(Node *t) {
+        t->sz = 1;
+        t->sum = t->key;
+        if (t->l) t->sz += t->l->sz, t->sum = f(t->l->sum, t->sum);
+        if (t->r) t->sz += t->r->sz, t->sum = f(t->sum, t->r->sum);
+    }
+
+    void rotr(Node *t) {
+        auto *x = t->p, *y = x->p;
+        if (x->l = t->r) t->r->p = x;
+        t->r = x, x->p = t;
+        update(x), update(t);
+        if (t->p = y) {
+            if (y->l == x) y->l = t;
+            if (y->r == x) y->r = t;
+            update(y);
+        }
+    }
+
+    void rotl(Node *t) {
+        auto *x = t->p, *y = x->p;
+        if (x->r = t->l) t->l->p = x;
+        t->l = x, x->p = t;
+        update(x), update(t);
+        if (t->p = y) {
+            if (y->l == x) y->l = t;
+            if (y->r == x) y->r = t;
+            update(y);
+        }
+    }
+
+    void splay(Node *t) {
+        push(t);
+        while (!t->is_root()) {
+            auto *q = t->p;
+            if (q->is_root()) {
+                push(q), push(t);
+                if (q->l == t) rotr(t);
+                else rotl(t);
             } else {
-                break;
+                auto *r = q->p;
+                push(r), push(q), push(t);
+                if (r->l == q) {
+                    if (q->l == t) rotr(q), rotr(t);
+                    else rotl(t), rotr(t);
+                } else {
+                    if (q->r == t) rotl(q), rotl(t);
+                    else rotr(t), rotl(t);
+                }
             }
         }
-        v->splay();
-        return make_pair(v->dmin, v->id);
     }
-    // vから根に向かうパスの中にある頂点の重みにxを加える
-    void addcost(int n, int x) {
-        node *v = expose(&nodes[n]);
-        v->dmin += x;
-    }
-    void cut(int v) {
-        node *c = &nodes[v];
-        expose(c);
-        node *p = c -> cp[L];
-        c->cp[L] = nullptr;
-        p->pp = nullptr;
 
-        p->dmin += c->dmin;
-        c->dmin += c->dcost;
-        c->dcost = 0;
-    }
-    void link(int v, int w) {
-        node *c = &nodes[v], *p = &nodes[w];
-        expose(c);
-        expose(p);
-        c->pp = p;
-        p->cp[R] = c;
-
-        p->dcost += p->dmin - min(p->dmin, c->dmin);
-        if (p->cp[L]) {
-            p->cp[L]->dmin += p->dmin - min(p->dmin, c->dmin);
+    Node *expose(Node *t) {
+        Node *rp = nullptr;
+        for (Node *cur = t; cur; cur = cur->p) {
+            splay(cur);
+            cur->r = rp;
+            update(cur);
+            rp = cur;
         }
-        p->dmin = min(p->dmin, c->dmin);
-        c->dmin = c->dmin - p->dmin;
+        splay(t);
+        return rp;
     }
-    void evert(int v) {
-        node *r = expose(&nodes[v]);
-        r->rev ^= true;
+
+    void link(Node *child, Node *parent) {
+        expose(child);
+        expose(parent);
+        child->p = parent;
+        parent->r = child;
+    }
+
+    void cut(Node *child) {
+        expose(child);
+        auto *parent = child->l;
+        child->l = nullptr;
+        parent->p = nullptr;
+    }
+
+    void evert(Node *t) {
+        expose(t);
+        toggle(t);
+        push(t);
+    }
+
+    Node *lca(Node *u, Node *v) {
+        expose(u);
+        return expose(v);
+    }
+
+    vi getpath(Node *x) {
+        vi vs;
+        function<void(Node *)> dfs = [&](Node *cur) {
+            if (!cur) return;
+            push(cur);
+            dfs(cur->r);
+            vs.push_back(cur->idx);
+            dfs(cur->l);
+        };
+        expose(x);
+        dfs(x);
+        return vs;
+    }
+
+    void set_propagate(Node *t, const T2 &x) {
+        expose(t);
+        propagate(t, x);
+        push(t);
     }
 };
 
 int main() {
-    cin.tie(0);
-    cout.tie(0);
-    ios::sync_with_stdio(0);
     int n;
     cin >> n;
-    LinkCutTree lct(n);
-    while (1) {
-        char cmd;
-        cin >> cmd;
-        if (cmd == 'C') {
-            int u, v;
-            cin >> u >> v;
-            u--;
-            v--;
-            lct.evert(u);
-            lct.link(u, v);
-        } else if (cmd == 'D') {
-            int u, v;
-            cin >> u >> v;
-            u--;
-            v--;
-            lct.evert(u);
-            lct.cut(v);
-        } else if (cmd == 'T') {
-            int u, v;
-            cin >> u >> v;
-            u--;
-            v--;
-            cout << (lct.findroot(u) == lct.findroot(v) ? "YES" : "NO") << endl; 
+    LinkCutTree<i64> lct([](i64 a, i64 b) {return a + b;}, 0);
+    vector<LinkCutTree<i64>::Node*> nodes(n);
+    for (int i = 0; i < n; i++) {
+        nodes[i] = lct.make_node(i);
+    }
+    for (int i = 0; i < n; i++) {
+        int d;
+        cin >> d;
+        for (int j = 0; j < d; j++) {
+            int k;
+            cin >> k;
+            lct.link(nodes[k], nodes[i]);
+        }
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int t;
+        cin >> t;
+        if (t == 0) {
+            int v, w;
+            cin >> v >> w;
+            lct.expose(nodes[v]);
+            nodes[v]->key += w;
         } else {
-            return 0;
+            int u;
+            cin >> u;
+            lct.expose(nodes[u]);
+            cout << nodes[u]->sum << endl;
         }
     }
 }
