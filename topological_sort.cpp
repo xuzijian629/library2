fe06022695ef7928e73b5c44e28a96504adbe524
Zijian Xu <xuzijian@g.ecc.u-tokyo.ac.jp>
2019-06-16 16:16:58 +0900

    update

--- topological_sort.cpp
+++ topological_sort.cpp
@@ -1,49 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-using i64 = int64_t;
-using vi = vector<i64>;
-using vvi = vector<vi>;
 
 class DAG {
     int n;
-    vvi adj;
-    vi used;
+    vector<vector<int>> adj;
+    vector<int> visited;
 
-public:
-    vi sorted;
-    DAG(int n) : n(n), adj(n), used(n) {}
-
-    void addArc(int a, int b) {
-        adj[a].push_back(b);
-    }
-    
-    void tsort() {
-        function<void(int)> dfs = [&](int v) {
-            if (used[v]) return;
-            used[v] = 1;
-            for (int s: adj[v]) {
+    void dfs(int v) {
+        if (visited[v] == 2) {
+            is_acyclic = false;
+            return;
+        } else if (!visited[v]) {
+            visited[v] = 2;
+            for (int s : adj[v]) {
                 dfs(s);
             }
+            visited[v] = 1;
             sorted.push_back(v);
-        };
+        }
+    }
+
+public:
+    vector<int> sorted;
+    DAG(int n) : n(n), adj(n), visited(n) {}
+    bool is_acyclic = true;
+
+    void add_arc(int a, int b) {
+        assert(0 <= a && a < n && 0 <= b && b < n);
+        adj[a].push_back(b);
+    }
+
+    void tsort() {
         for (int i = 0; i < n; i++) {
-            dfs(i);
+            if (!visited[i]) dfs(i);
         }
         reverse(sorted.begin(), sorted.end());
     }
 };
-
-int main() {
-    int v, e;
-    cin >> v >> e;
-    DAG g(v);
-    for (int i = 0; i < e; i++) {
-        int a, b;
-        cin >> a >> b;
-        g.addArc(a, b);
-    }
-    g.tsort();
-    for (int v: g.sorted) {
-        cout << v << endl;
-    }
-}
